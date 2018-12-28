3ef661f6b5a27395727c4088c969a1108b5aa916
sildar <sildar44@gmail.com>
2018-12-28 16:38:54 +0100

    Improve setup file for pypi upload

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
     name = "potara",
-    version = "0.1.0",
+    version = "1.0.0",
     author = "Remi Bois",
     author_email = "remibois.contact@gmail.com",
     description = ("A multi-document summarizer based on ILP and sentence fusion."),
@@ -18,6 +20,26 @@ setup(
     packages = ['potara', 'tests'],
     test_suite =  "tests.test_all",
     long_description=read('README.md'),
-    classifiers=[
-    ],
+    long_description_content_type="text/markdown",
+    url='https://github.com/sildar/potara',
+    classifiers=['Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Scientific/Engineering :: Information Analysis',
+                 'Topic :: Text Processing :: Linguistic',
+                ],
+    install_requires=['gensim==3.6.0',
+                      'networkx==1.8.1',
+                      'nltk==3.4',
+                      'pulp',
+                      'setuptools==40.6.3',
+                      'python-coveralls==2.4.2',
+                      'six==1.12.0',
+                      'requests>=2.21.0',
+                      ]
 )
